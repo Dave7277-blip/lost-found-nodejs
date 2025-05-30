f1b0b69b5f6d9f9ad7c31d93c4c39fd3c595baa6
Victor-keche <117376597+Victor-keche@users.noreply.github.com>
2025-05-30 11:27:16 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,14 @@
-const mysql = require('mysql2');
+// backend/db.js
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'lostfound'
+const pool = mysql.createPool({
+  host: process.env.DB_HOST, // Should be trolley_proxy.rlwy.net
+  port: process.env.DB_PORT, // From Railway (not 3306)
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL database');
-});
-
-module.exports = connection;
+module.exports = pool;
