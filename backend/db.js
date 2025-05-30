b509e9dffa34a0fc1db378f8469d77a2951b80bb
Victor-keche <117376597+Victor-keche@users.noreply.github.com>
2025-05-30 12:06:32 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'mysql://root:pAJygxdfaLILeYHrAOsTjhCuwyrNkfgf@shuttle.proxy.rlwy.net:11189/railway', // Should be trolley_proxy.rlwy.net
+  host: 'shuttle.proxy.rlwy.net', // Should be trolley_proxy.rlwy.net
   port: 3306, // From Railway (not 3306)
-  user: root,
+  user: 'root',
   password: pAJygxdfaLILeYHrAOsTjhCuwyrNkfgf,
   database: railway,
   waitForConnections: true,
